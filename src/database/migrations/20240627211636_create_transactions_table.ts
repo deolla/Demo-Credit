603a636c8cbdec4251248dd782b4a5ad74ed586f
Adeosun Oluwaseyi <deosundeola@gmail.com>
2024-07-01 00:39:48 +0100

    updates

--- src/database/migrations/20240627211636_create_transactions_table.ts
+++ src/database/migrations/20240627211636_create_transactions_table.ts
@@ -5,7 +5,7 @@ export async function up(knex: Knex): Promise<void> {
     return knex.schema.createTable('transactions', (table) => {
         table.uuid('id').primary().notNullable().unique()
         table.uuid('from_walletId').nullable().references('id').inTable('wallets').onDelete('CASCADE');
-        table.uuid('to_walletId').notNullable().references('id').inTable('wallets').onDelete('CASCADE');
+        table.uuid('to_walletId').nullable().references('id').inTable('wallets').onDelete('CASCADE');
         table.decimal('amount', 10, 2).notNullable();
         table.string('description').nullable();
         table.string('currency').nullable().defaultTo('NGN');
--- src/server.ts
+++ src/server.ts
@@ -9,8 +9,7 @@ import walletRouter from './routes/walletRoute';
 
 dotenv.config();
 const app = express();
-//const PORT = process.env.PORT || 3000;
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
